36f07418f32ffc8a34d4c98ba64b1d8a37276cc2
ole-thoeb <43213977+ole-thoeb@users.noreply.github.com>
2018-11-06 17:02:48 +0100

    paar fixes bei der 4

--- Blatt_3/emil/4.c
+++ Blatt_3/emil/4.c
@@ -5,7 +5,7 @@ float f(){
 	float x2 = -0.001f / 9.0f;
 	float x3 = 2500.0f;
 	float x4 = 0.001f / 7.0f;
-	float x5 = -0.0125f;
+	float x5 = -12500.0;
 
 	return x1 + x2 + x3 + x4 + x5;
 }
@@ -15,13 +15,13 @@ double d(){
 	double x2 = (double)-0.001 / (double)9.0;
 	double x3 = (double)2500.0;
 	double x4 = (double)0.001 / (double)7.0;
-	double x5 = (double)-0.0125;
+	double x5 = (double)-12500.0;
 
 	return x1 + x2 + x3 + x4 + x5;
 }
 
 float sf(){
-	float arg[5]= {10000.0f, (-0.001f / 9.0f), 2500.0f, (0.001f / 7.0f), -0.0125f};
+	float arg[5]= {10000.0f, (-0.001f / 9.0f), 2500.0f, (0.001f / 7.0f), -12500.0f};
 
 	float s = 0.0;
 	float d = 0.0;
@@ -46,6 +46,8 @@ int main(int argc, char const *argv[])
 
 
 /*
-7,199,992,145 / 630,000
-7199992145
+a) 2 / 63000
+b) 0.000032
+c) 0.000032
+d) 
 */
